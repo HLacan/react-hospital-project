ded142002fd3cb2c2c4491dfdc4aeff1b1811581
HLacan <3017517650101@ingenieria.usac.edu.gt>
2022-04-23 20:32:06 -0600

    add mock and api

---
+++ tools/apiServer.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+
+const middlewares = jsonServer.defaults({
+  static: 'node_modules/json-server/dist'
+})
+
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+server.use(router)
+server.listen(3001, () => {
+  console.log('Server running on port 3001!')
+})
---
+++ tools/mockData.js
@@ -0,0 +1,27 @@
+const doctors = [
+  {
+    id: 1,
+    DPI: '1234567890101',
+    name: 'Jeorge',
+    lastName: 'Herrera',
+    phoneNumber: '12345844'
+  },
+  {
+    id: 2,
+    DPI: '9876543210101',
+    name: 'Herbert',
+    lastName: 'Lacan',
+    phoneNumber: '78549824'
+  },
+  {
+    id: 2,
+    DPI: '4818481840101',
+    name: 'Andrea',
+    lastName: 'Hurtado',
+    phoneNumber: '68750570'
+  }
+]
+
+module.exports = {
+  doctors
+}
