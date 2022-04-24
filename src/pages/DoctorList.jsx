72d78840e0156c4305b398495b6db491b430b7ae
HLacan <3017517650101@ingenieria.usac.edu.gt>
2022-04-23 21:42:28 -0600

    add doctors json and modify mock

--- src/pages/DoctorList.jsx
+++ src/pages/DoctorList.jsx
@@ -1,4 +1,28 @@
-import { Table, TableContainer, Tbody, Text, Th, Thead, Tr } from "@chakra-ui/react"
+import { Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react"
+
+const doctors = [
+  {
+    id: 1,
+    dpi: '10',
+    name: 'Jeorge',
+    lastName: 'Herrera',
+    phoneNumber: '12345844'
+  },
+  {
+    id: 2,
+    dpi: '20',
+    name: 'Herbert',
+    lastName: 'Lacan',
+    phoneNumber: '78549824'
+  },
+  {
+    id: 2,
+    dpi: '30',
+    name: 'Andrea',
+    lastName: 'Hurtado',
+    phoneNumber: '68750570'
+  }
+]
 
 export default function DoctorList() {
   return (
@@ -17,7 +41,15 @@ export default function DoctorList() {
             </Tr>
           </Thead>
           <Tbody>
-
+            {doctors.map((ticket) => (
+              <Tr>
+                <Td>{ticket.id}</Td>
+                <Td>{ticket.dpi}</Td>
+                <Td>{ticket.name}</Td>
+                <Td>{ticket.lastName}</Td>
+                <Td>{ticket.phoneNumber}</Td>
+              </Tr>
+            ))}
           </Tbody>
         </Table>
       </TableContainer>
