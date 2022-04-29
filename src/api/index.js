1b3a5afd24aaef0f34c99c08bddb8456e5379510
HLacan <3017517650101@ingenieria.usac.edu.gt>
2022-04-28 19:57:08 -0600

    change loading time to 0

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 const SERVER_URL = 'http://localhost:3001'
 
 export const api = async (service, options) => {
-  await new Promise(resolve => setTimeout(resolve, 3000))
+  await new Promise(resolve => setTimeout(resolve, 0))
   return fetch(`${SERVER_URL}/${service}`, options).then(res => res.json())
 }
