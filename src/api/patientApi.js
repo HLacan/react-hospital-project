f7f187c652ffcacd78f5b59d52eb25adf296bdee
HLacan <3017517650101@ingenieria.usac.edu.gt>
2022-04-28 19:56:08 -0600

    add POST PUT DELETE method

--- src/api/patientApi.js
+++ src/api/patientApi.js
@@ -3,3 +3,17 @@ import { api } from 'api'
 export const getPatients = () => api('patients')
 
 export const getPatient = id => api(`patients/${id}`)
+
+export const savePatient = patient =>
+  api(`patients${patient.id ? `/${patient.id}` : ''}`, {
+    method: patient.id ? 'PUT' : 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(patient),
+  })
+
+export const deletePatient = async id =>
+  api(`patients/${id}`, {
+    method: 'DELETE',
+  })
