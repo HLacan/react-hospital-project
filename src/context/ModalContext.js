d85b918824f891d39e3c82802c6b6214e8d2872a
HLacan <3017517650101@ingenieria.usac.edu.gt>
2022-04-30 13:56:31 -0600

    add patients to context

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -4,9 +4,29 @@ export const ModalContext = createContext({})
 
 export const ModalProvider = ({ children }) => {
   const [openModal, setOpenModal] = useState()
+  const [editingValue, setEditingValue] = useState()
+
+  const isPatientModalOpen = openModal === 'patients'
+
+  const openPatientModal = value => {
+    setEditingValue(value)
+    setOpenModal('patients')
+  }
+
+  const closeModal = () => {
+    setEditingValue({})
+    setOpenModal()
+  }
 
   return (
-    <ModalContext.Provider value={{ openModal, setOpenModal }}>
+    <ModalContext.Provider
+      value={{
+        editingValue,
+        isPatientModalOpen,
+        openPatientModal,
+        closeModal,
+      }}
+    >
       {children}
     </ModalContext.Provider>
   )
