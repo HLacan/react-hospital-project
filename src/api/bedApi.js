4f5e7d201131fdf423676da13be082eb71ce828e
HLacan <3017517650101@ingenieria.usac.edu.gt>
2022-05-11 16:17:54 -0600

    add bed api

---
+++ src/api/bedApi.js
@@ -0,0 +1,17 @@
+import { api } from 'api'
+
+export const getAreaBeds = id => api(`areas/${id}/beds`)
+
+export const saveBed = bed =>
+  api(`beds${bed.id ? `/${bed.id}` : ''}`, {
+    method: bed.id ? 'PUT' : 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(bed),
+  })
+
+export const deleteBed = async id =>
+  api(`beds/${id}`, {
+    method: 'DELETE',
+  })
