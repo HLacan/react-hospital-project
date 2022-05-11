43b512ccd5a166ae215ba8b28a8a539893d746cb
HLacan <3017517650101@ingenieria.usac.edu.gt>
2022-05-11 10:35:33 -0600

    add options and area id

---
+++ src/components/BedFormModal.jsx
@@ -0,0 +1,94 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  VisuallyHidden,
+} from '@chakra-ui/react'
+import { saveBed } from 'api/bedApi'
+import Input from 'components/Input'
+import Select from 'components/Select'
+import useForm from 'hooks/useForm'
+import useModalContext from 'hooks/useModalContext'
+import queryClient from 'queryClient'
+import { useEffect } from 'react'
+import { useMutation } from 'react-query'
+
+export default function BedFormModal() {
+  const { isBedModalOpen, editingValue, closeModal } = useModalContext()
+
+  const { values, handleChange, handleSubmit, updateValues } = useForm({
+    status: '',
+    areaId: 0,
+  })
+
+  const list = [
+    {
+      value: 'libre',
+      label: 'Libre',
+    },
+    {
+      value: 'ocupado',
+      label: 'Ocupado',
+    },
+  ]
+
+  const { mutate, isLoading } = useMutation(saveBed, {
+    onSuccess: data => {
+      queryClient.invalidateQueries('beds')
+      queryClient.invalidateQueries(['beds', String(data.id)])
+      closeModal()
+    },
+  })
+
+  useEffect(() => {
+    if (editingValue) {
+      updateValues(editingValue)
+    }
+  }, [editingValue, updateValues])
+
+  return (
+    <Modal isOpen={isBedModalOpen} onClose={closeModal}>
+      <ModalOverlay />
+
+      <ModalContent>
+        <form onSubmit={handleSubmit(mutate)}>
+          <ModalHeader>Crear Cama {isLoading && <Spinner />}</ModalHeader>
+
+          <ModalCloseButton />
+
+          <ModalBody>
+            <Select
+              name='status'
+              label='Estado'
+              onChange={handleChange}
+              options={list}
+              value={values.status}
+            ></Select>
+
+            <VisuallyHidden>
+              <Input
+                isDisabled={true}
+                name='areaId'
+                label='Area'
+                onChange={handleChange}
+                value={values.areaId}
+              />
+            </VisuallyHidden>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' disabled={isLoading} type='submit'>
+              Guardar
+            </Button>
+          </ModalFooter>
+        </form>
+      </ModalContent>
+    </Modal>
+  )
+}
