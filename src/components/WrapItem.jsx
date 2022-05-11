6d24ddb377355dbf9175082f766a4080b6ac17aa
HLacan <3017517650101@ingenieria.usac.edu.gt>
2022-05-11 10:38:06 -0600

    modified style

--- src/components/WrapItem.jsx
+++ src/components/WrapItem.jsx
@@ -3,9 +3,9 @@ import { Box, Divider, Text, WrapItem as UIWrapItem } from '@chakra-ui/react'
 export default function WrapItem({ ...props }) {
   return (
     <UIWrapItem>
-      <Box w='300px' h='full' p='4' bg='gray.200' borderRadius='xl'>
+      <Box w='300px' h='full' p='4' bg='#E4DECB' borderRadius='xl'>
         <Text fontSize='xl'>{props.title}</Text>
-        <Divider borderColor='black' padding={1} />
+        <Divider borderColor='black' padding={1} marginBottom={3} />
         <Text fontSize='md'>{props.children}</Text>
       </Box>
     </UIWrapItem>
