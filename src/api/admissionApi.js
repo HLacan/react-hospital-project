1936bab59121f673f827097fe21f4b6837246363
HLacan <3017517650101@ingenieria.usac.edu.gt>
2022-05-12 01:54:42 -0600

    add admission api

--- src/api/admissionApi.js
+++ src/api/admissionApi.js
@@ -1,3 +1,17 @@
 import { api } from 'api'
 
 export const getPatientAdmissions = id => api(`patients/${id}/admissions`)
+
+export const saveAdmission = admission =>
+  api(`admission${admission.id ? `/${admission.id}` : ''}`, {
+    method: admission.id ? 'PUT' : 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(admission),
+  })
+
+export const deleteAdmission = async id =>
+  api(`admissions/${id}`, {
+    method: 'DELETE',
+  })
