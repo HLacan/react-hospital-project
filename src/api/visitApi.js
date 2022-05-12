7daa18674caff3ceaa54b973f0c9cc10e9852811
HLacan <3017517650101@ingenieria.usac.edu.gt>
2022-05-12 01:54:15 -0600

    add save and delete method

--- src/api/visitApi.js
+++ src/api/visitApi.js
@@ -1,3 +1,17 @@
 import { api } from 'api'
 
 export const getPatientVisits = id => api(`patients/${id}/visits`)
+
+export const saveVisit = visit =>
+  api(`visits${visit.id ? `/${visit.id}` : ''}`, {
+    method: visit.id ? 'PUT' : 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(visit),
+  })
+
+export const deleteVisit = async id =>
+  api(`visits/${id}`, {
+    method: 'DELETE',
+  })
